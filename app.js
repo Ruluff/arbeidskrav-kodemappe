159346b95a6a97eb487a60080b4221cf8c5d4112
ruluff <tony.moen94@gmail.com>
2025-02-08 09:51:33 +0100

    createcharacter

--- app.js
+++ app.js
@@ -3,6 +3,44 @@
 
 // Del 1: Lag karakter og lagre karakteren i localStorage
 
+const characterCreator = getElementById("character-creator");
+
+function characterCreator() {
+document.getElementById("profile-img").src = "assets/death-knight.jpeg";
+document.getElementById("character-name").innerHTML = "test";
+}
+
+document.getElementById("character-button").addEventListener("click", characterCreator);
+
+
+/*
+
+// Character HP
+const characterHP = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+
+const getCharacterHP = characterHP(50, 151);
+
+console.log(getCharacterHP);
+
+// Character attack damage
+const attackDamage = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+
+const getAttackDamage = attackDamage(10, 51);
+
+console.log(getAttackDamage);
+
+// Create Character
+
+function createNewCharacter() {
+  const createCharacter = getElementByID("create-character");
+}
+
+console.log(createNewCharacter);
+
 //Seksjon 2: Generer fiende
 
 // Seksjon 3: Sloss!
