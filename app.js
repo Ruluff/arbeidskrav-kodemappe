58f26f391c0e8ee9e8e447f81f5d1a44c7ae3290
ruluff <tony.moen94@gmail.com>
2025-02-12 22:28:11 +0100

    Display and fight

--- app.js
+++ app.js
@@ -5,18 +5,17 @@
 
 let selectedProfilePicture = 0; // lagt til 0 for å velge en karakter uansett
 
+const profilePicture = Array.from(document.getElementsByClassName("profile-img"));
+profilePicture.forEach(function (image, index) {
+  image.addEventListener("click", function () {
+    selectedProfilePicture = index;
+  });
+});
+
 function createCharacter() {
   const characterName = document.getElementById("character-name").value;
   const characterHp = parseInt(document.getElementById("character-hp").value, 10);
   const attackDamage = parseInt(document.getElementById("attack-damage").value, 10);
-
-  const profilePicture = Array.from(document.getElementsByClassName("profile-img"));
-
-  profilePicture.forEach(function (image, index) {
-    image.addEventListener("click", function () {
-      selectedProfilePicture = index;
-    });
-  });
 
   const character = {characterName, characterHp, attackDamage, profilePicture: selectedProfilePicture};
 
@@ -28,7 +27,7 @@ function createCharacter() {
 
 document.getElementById("create-character").addEventListener("click", createCharacter);
 
-//Seksjon 2: Generer fiende
+//Seksjon 2: Genrer fiende
 
 function enemyCreator() {
   const enemies = [
@@ -59,4 +58,115 @@ function enemyCreator() {
 
 document.getElementById("enemy-creator").addEventListener("click", enemyCreator);
 
-// Seksjon 3: Sloss!
+// Seksjon 3: Sloss
+
+// Slosskamp insp: https://github.com/ZakSchenck/pokemon-game/blob/main/app.js
+
+function battle(charHp, charAttack, enemyHp, enemyAttack) {
+  const remainingCharHp = charHp - enemyAttack;
+  const remainingEnemyHp = enemyHp - charAttack;
+
+  if (remainingCharHp === remainingEnemyHp) {
+    return "Uavgjort";
+  } else if (remainingCharHp > remainingEnemyHp) {
+    return "Du vant!";
+  } else {
+    return "Du tapte!";
+  }
+}
+
+function chooseWinner() {
+  const createdCharacter = JSON.parse(localStorage.getItem("character"));
+  const createdEnemy = JSON.parse(localStorage.getItem("randomEnemy"));
+  const resultMessage = battle(
+    createdCharacter.characterHp,
+    createdCharacter.attackDamage,
+    createdEnemy.hp,
+    createdEnemy.attack
+  );
+  document.getElementById("battle-result").innerHTML = resultMessage;
+}
+
+function startFight() {
+  const createdCharacter = JSON.parse(localStorage.getItem("character"));
+  const createdEnemy = JSON.parse(localStorage.getItem("randomEnemy"));
+
+  chooseWinner(createdCharacter.characterHp, createdEnemy.hp);
+
+  const battleArea = document.getElementById("battle-area");
+
+  chooseWinner();
+
+  // Display
+
+  const imageArray = ["assets/death-knight.jpeg", "assets/hunter.jpg", "assets/mage.webp"];
+
+  const characterDisplay = document.createElement("div");
+  characterDisplay.id = "character-display";
+  characterDisplay.className = "profile-card";
+
+  const charHeading = document.createElement("h2");
+  charHeading.textContent = "Helten";
+  characterDisplay.appendChild(charHeading);
+
+  const charImg = document.createElement("img");
+  charImg.id = "char-img";
+  charImg.src = imageArray[createdCharacter.profilePicture];
+  characterDisplay.appendChild(charImg);
+
+  const charName = document.createElement("p");
+  charName.id = "char-name";
+  charName.textContent = createdCharacter.characterName;
+  characterDisplay.appendChild(charName);
+
+  const charHp = document.createElement("p");
+  charHp.id = "char-hp";
+  charHp.textContent = createdCharacter.characterHp;
+  characterDisplay.appendChild(charHp);
+
+  const charAttack = document.createElement("p");
+  charAttack.id = "char-attack";
+  charAttack.textContent = createdCharacter.attackDamage;
+  characterDisplay.appendChild(charAttack);
+
+  // enemy display
+
+  const enemyFightDisplay = document.createElement("div");
+  enemyFightDisplay.id = "enemy-fight-display";
+  enemyFightDisplay.className = "profile-card";
+
+  const enemyHeading = document.createElement("h2");
+  enemyHeading.textContent = "Fienden";
+  enemyFightDisplay.appendChild(enemyHeading);
+
+  const enemyFightImg = document.createElement("img");
+  enemyFightImg.id = "enemy-fight-img";
+  enemyFightImg.src = createdEnemy.img;
+  enemyFightDisplay.appendChild(enemyFightImg);
+
+  const enemyFightName = document.createElement("p");
+  enemyFightName.id = "enemy-fight-name";
+  enemyFightName.textContent = createdEnemy.name;
+  enemyFightDisplay.appendChild(enemyFightName);
+
+  const enemyFightHp = document.createElement("p");
+  enemyFightHp.id = "enemy-fight-hp";
+  enemyFightHp.textContent = createdEnemy.hp;
+  enemyFightDisplay.appendChild(enemyFightHp);
+
+  const enemyFightAttack = document.createElement("p");
+  enemyFightAttack.id = "enemy-fight-attack";
+  enemyFightAttack.textContent = createdEnemy.attack;
+  enemyFightDisplay.appendChild(enemyFightAttack);
+
+  battleArea.appendChild(characterDisplay);
+  battleArea.appendChild(enemyFightDisplay);
+
+  console.log(startFight);
+}
+
+document.getElementById("start-fight").addEventListener("click", startFight);
+
+// resultat
+
+// function chooseWinner(subtract) {
